6b3a9f114a984bd8531fe25a99d2f1b8ab0f1abf
Don Cuong Tran <donct@uci.edu>
2018-12-05 03:46:05 -0800

    Generates PDF file as of right now

--- GameSIG/createmypdf.php
+++ GameSIG/createmypdf.php
@@ -93,58 +93,6 @@ if (!empty($_POST['submit']))
 
 	$pdf->output("", $file);
 
-/* Email Detials */
-  $mail_to = "donct@uci.edu";
-  $from_mail = "donct@uci.edu";
-  $from_name = "Don Cuong Tran";
-  $reply_to = "donct@uci.edu";
-  $subject = "GameSIG Registration Form";
-  $message = "Here's my form.";
- 
-/* Attachment File */
-  $file_size = filesize($file);
-  $handle = fopen($file, "r");
-  $content = fread($handle, $file_size);
-  fclose($handle);
-  $content = chunk_split(base64_encode($content));
-   
-/* Set the email header */
-  // Generate a boundary
-  $boundary = md5(uniqid(time()));
-   
-  // Email header
-  $header = "From: ".$from_name." <".$from_mail.">".PHP_EOL;
-  $header .= "Reply-To: ".$reply_to.PHP_EOL;
-  $header .= "MIME-Version: 1.0".PHP_EOL;
-   
-  // Multipart wraps the Email Content and Attachment
-  $header .= "Content-Type: multipart/mixed; boundary=\"".$boundary."\"".PHP_EOL;
-  $header .= "This is a multi-part message in MIME format.".PHP_EOL;
-  $header .= "--".$boundary.PHP_EOL;
-   
-  // Email content
-  // Content-type can be text/plain or text/html
-  $header .= "Content-type:text/plain; charset=iso-8859-1".PHP_EOL;
-  $header .= "Content-Transfer-Encoding: 7bit".PHP_EOL.PHP_EOL;
-  $header .= "$message".PHP_EOL;
-  $header .= "--".$boundary.PHP_EOL;
-   
-  // Attachment
-  // Edit content type for different file extensions
-  $header .= "Content-Type: application/xml; name=\"".$file_name."\"".PHP_EOL;
-  $header .= "Content-Transfer-Encoding: base64".PHP_EOL;
-  $header .= "Content-Disposition: attachment; filename=\"".$file_name."\"".PHP_EOL.PHP_EOL;
-  $header .= $content.PHP_EOL;
-  $header .= "--".$boundary."--";
-   
-  // Send email
-  if (mail($mail_to, $subject, "", $header)) {
-    echo "Sent";
-  } else {
-    echo "Error";
-  }
-
-
 }
 
 ?>
