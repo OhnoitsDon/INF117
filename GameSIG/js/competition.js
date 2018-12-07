727b2dc9a98034d5000552f72cfdc4669f545b56
Don Cuong Tran <donct@uci.edu>
2018-12-06 19:40:51 -0800

    Bolded clickable text

--- GameSIG/js/competition.js
+++ GameSIG/js/competition.js
@@ -49,19 +49,15 @@ $(window).scroll(function() {
    }
 });
 
-
-
-
-
-   function scrollFunction() {
-       if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-           document.getElementById("myBtn").style.display = "block";
-       } else {
-           document.getElementById("myBtn").style.display = "none";
-       }
+function scrollFunction() {
+   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+       document.getElementById("myBtn").style.display = "block";
+   } else {
+       document.getElementById("myBtn").style.display = "none";
    }
+}
 
-   function topFunction() {
-       document.body.scrollTop = 0;
-       document.documentElement.scrollTop = 0;
-   }
+function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+}
